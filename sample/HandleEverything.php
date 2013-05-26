439737e4747065c2b296706a5956ff36155133a4
Szurovecz János <szjani@szjani.hu>
2013-05-26 17:27:48 +0200

    more samples

---
+++ sample/HandleEverything.php
@@ -0,0 +1,32 @@
+<?php
+namespace sample;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use precore\lang\Object;
+use predaddy\eventhandling\DirectEventBus;
+use predaddy\eventhandling\Event;
+use predaddy\eventhandling\EventBase;
+use predaddy\eventhandling\EventHandler;
+
+class SampleEvent1 extends EventBase
+{
+}
+
+class SampleEvent2 extends EventBase
+{
+}
+
+class AllEventHandler extends Object implements EventHandler
+{
+    public function handleEvents(Event $event)
+    {
+        printf("Instance of %s has been caught\n", $event->getClassName());
+    }
+}
+
+$bus = new DirectEventBus('sample3');
+$bus->register(new AllEventHandler());
+
+$bus->post(new SampleEvent1());
+$bus->post(new SampleEvent2());
