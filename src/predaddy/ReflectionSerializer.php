4c390c1976cb14680fd9b504f4b8c3729d3ec51e
Szurovecz János <szjani@szjani.hu>
2014-02-24 22:23:53 +0100

    serializer interface and implementation added

---
+++ src/predaddy/ReflectionSerializer.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ * Copyright (c) 2012-2014 Szurovecz János
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace predaddy;
+
+use precore\lang\ObjectInterface;
+use ReflectionProperty;
+
+class ReflectionSerializer implements Serializer
+{
+    /**
+     * @var array
+     */
+    private $blackListProperties;
+
+    /**
+     * @param array $blackListProperties
+     */
+    public function __construct(array $blackListProperties = array())
+    {
+        $this->blackListProperties = $blackListProperties;
+    }
+
+    /**
+     * @param ObjectInterface $object
+     * @return string
+     */
+    public function serialize(ObjectInterface $object)
+    {
+        $array = array();
+        $properties = $object->getObjectClass()->getProperties();
+        /* @var $property \ReflectionProperty */
+        foreach ($properties as $property) {
+            if ($this->isSerializable($property)) {
+                $property->setAccessible(true);
+                $array[$property->getName()] = $property->getValue($object);
+            }
+        }
+        return serialize($array);
+    }
+
+    /**
+     * @param string $serialized
+     * @param ObjectInterface $outputObject
+     * @return ObjectInterface
+     */
+    public function deserialize($serialized, ObjectInterface $outputObject = null)
+    {
+        $array = unserialize($serialized);
+        $outputType = $outputObject->getObjectClass();
+        $properties = $outputType->getProperties();
+        $result = $outputObject ?: $outputType->newInstanceWithoutConstructor();
+        /* @var $property \ReflectionProperty */
+        foreach ($properties as $property) {
+            if ($this->isSerializable($property) && array_key_exists($property->getName(), $array)) {
+                $property->setAccessible(true);
+                $property->setValue($result, $array[$property->getName()]);
+            }
+        }
+        return $result;
+    }
+
+    protected function isSerializable(ReflectionProperty $property)
+    {
+        return !in_array($property->getName(), $this->blackListProperties);
+    }
+}
