b1ab95bb1dbf3b812adc0c8a2ef829a28d5b2cf4
Szurovecz János <szjani@szjani.hu>
2014-03-06 21:50:23 +0100

    ReflectionSerializer test added

---
+++ tests/src/predaddy/serializer/Foo.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Copyright (c) 2012-2014 Szurovecz János
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace predaddy\serializer;
+
+use precore\lang\Object;
+
+class Foo extends Object
+{
+    private $fooPrivate;
+    protected $fooProtected;
+    public $fooPublic;
+
+    public function __construct()
+    {
+        $this->fooPrivate = '001fooPrivate';
+        $this->fooProtected = '002fooProtected';
+        $this->fooPublic = '003fooPublic';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFooPrivate()
+    {
+        return $this->fooPrivate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFooProtected()
+    {
+        return $this->fooProtected;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFooPublic()
+    {
+        return $this->fooPublic;
+    }
+}
---
+++ tests/src/predaddy/serializer/ReflectionSerializerTest.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Copyright (c) 2012-2014 Szurovecz János
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace predaddy\serializer;
+
+use PHPUnit_Framework_TestCase;
+
+require_once 'Foo.php';
+require_once 'Bar.php';
+
+class ReflectionSerializerTest extends PHPUnit_Framework_TestCase
+{
+    public function testSerialize()
+    {
+        $serializer = new ReflectionSerializer();
+        $obj = new Bar();
+        $serialized = $serializer->serialize($obj);
+        /* @var $newObj Bar */
+        $newObj = $serializer->deserialize($serialized, Bar::objectClass());
+
+        self::assertEquals($obj->getBarPrivate(), $newObj->getBarPrivate());
+        self::assertEquals($obj->getBarProtected(), $newObj->getBarProtected());
+        self::assertEquals($obj->getBarPublic(), $newObj->getBarPublic());
+
+        self::assertEquals($obj->getFooPublic(), $newObj->getFooPublic());
+        self::assertEquals($obj->getFooProtected(), $newObj->getFooProtected());
+        self::assertNotEquals($obj->getFooPrivate(), $newObj->getFooPrivate());
+        self::assertNull($newObj->getFooPrivate());
+    }
+}
