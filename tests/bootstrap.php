407020d43fbc8565223c279fdfddbc110e8a3d18
Szurovecz János <szjani@szjani.hu>
2014-04-16 19:30:58 +0200

    autoload classes under tests directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    include_once __DIR__ . '/../vendor/autoload.php';
+    $loader = include_once __DIR__ . '/../vendor/autoload.php';
+    $loader->add('predaddy', __DIR__ . '/src');
 }
 Logger::configure(__DIR__ . '/src/resources/log4php.xml');
