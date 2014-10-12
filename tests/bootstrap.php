ca7dc07f648a309954c6965715e5c290ddcf9b27
Janos Szurovecz <szjani@szjani.hu>
2014-10-12 10:42:41 +0200

    annotation caching in unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,9 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     $loader->add('predaddy', __DIR__ . '/src');
 }
 Logger::configure(__DIR__ . '/src/resources/log4php.xml');
+\predaddy\messagehandling\annotation\AnnotatedMessageHandlerDescriptor::setReader(
+    new \Doctrine\Common\Annotations\CachedReader(
+        new \Doctrine\Common\Annotations\AnnotationReader(),
+        new \Doctrine\Common\Cache\ArrayCache()
+    )
+);
